d763732bb668630596880b7322d275b4f24a6a51
KavithaMaharajan-svg <Kavitha.Maharajan@cognizant.com>
2026-03-11 00:19:43 +0530

    Update streamlit_app.py
    updated ingredient_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,10 @@ ingredients_string=''
 if ingredients_list:   
    ingredients_string=''
  
-   for fruits_chosen in ingredients_list:
+   for fruit_chosen in ingredients_list:
        ingredients_string += fruits_chosen +' '
- 
+       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/watermelon")
+     sf.df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
  
 #st.write(ingredients_string)
 my_insert_stmt = """ insert into smoothies.public.orders(INGREDIENTS,NAME_ON_ORDER)
