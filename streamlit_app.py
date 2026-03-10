75869e00255b69744c36f1fa0c1cd12f42da3911
KavithaMaharajan-svg <Kavitha.Maharajan@cognizant.com>
2026-03-11 00:25:27 +0530

    Update streamlit_app.py
    updated code

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ ingredients_string=''
 if ingredients_list:   
    ingredients_string=''
  
-   for fruit_chosen in ingredients_list:
+   for fruits_chosen in ingredients_list:
        ingredients_string += fruits_chosen +' '
        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/watermelon")
        sf.df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
