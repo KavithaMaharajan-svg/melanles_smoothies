2d28e2fb57871e578f98e2fb793c7b7c6de05863
KavithaMaharajan-svg <Kavitha.Maharajan@cognizant.com>
2026-03-11 00:37:45 +0530

    Update streamlit_app.py
    updated code

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,11 +42,6 @@ st.write(my_insert_stmt)
 time_to_insert = st.button('Submit_order')
 
 if time_to_insert:
-
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon="✅")
 
- 
-#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-#sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
